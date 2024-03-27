186492e8c046534c1ad9425d1bd42d4c17b85b49
techpepson <techpepson@gmail.com>
2024-03-27 06:10:30 +0000

    feat(Auth Page): Design the login page for student and teachers, devoid of functionalities.

---
+++ src/frontend-source-code/components/AUTH_PAGES/auth -interface.ts
@@ -0,0 +1,10 @@
+export interface AuthPosition {
+  flexPosition: string;
+  widthDefinition: string;
+  colFlex: string;
+  xlText: string;
+  shadow: string;
+  cursor: string;
+  rawFlex: string;
+  
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Router>
     <React.StrictMode>
-      <App />
+      <Theme accentColor="gold">
+        <App />
+      </Theme>
     </React.StrictMode>
   </Router>
 );
